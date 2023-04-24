ea32551a45bbb52f09d61a0df3a91c61a7db4181
slovohot <antoshkamc.a@gmail.com>
2023-04-24 15:59:43 +0500

    edits brain-gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -9,12 +9,7 @@ GAME_RULE = 'Find the greatest common divisor of given numbers.'
 def get_game():
     rand_num_one = random.randint(1, 10)
     rand_num_two = random.randint(1, 20)
-    # создаем 2 списка делителей
-    list_num_one = gcd(rand_num_one)
-    list_num_two = gcd(rand_num_two)
-    # создаем один общий список делителей
-    identical_divisors = list(set(list_num_one) & set(list_num_two))
-    # ищем максимальный делитель
-    correct_answer = max(identical_divisors)
+    # ищем НОД
+    correct_answer = gcd(rand_num_one, rand_num_two)
     question = str(rand_num_one) + " " + str(rand_num_two)
     return question, correct_answer
