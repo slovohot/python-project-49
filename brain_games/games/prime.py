9ec6262a81369865fe6d6afb58f9aa71f0997cfe
slovohot <antoshkamc.a@gmail.com>
2023-04-24 16:08:40 +0500

    edits brain-prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,19 +5,20 @@ from math import gcd
 GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-# является ли число простым
-def is_prime(num):
-    if len(gcd(num)) == 2:
-        return num
+def find_greatest_divisor(n):
+    divisor = []
+    for i in range(1, int(n) + 1):
+        if n % i == 0:
+            divisor += [i]
+    return divisor
 
 
 # Функция вычислений, правильного вопроса и ответа
 def get_game():
-    rand_num_one = 1
-    rand_num_two = 10
-    random_number = random.randint(rand_num_one, rand_num_two)
+    random_number = random.randint(1, 20)
     question = random_number
-    if is_prime(random_number):
+    divisor = find_greatest_divisor(random_number)
+    if len(divisor) == 2:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
